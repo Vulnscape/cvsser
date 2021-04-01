18a23fca779fe05e8c5e785ac2cb5bbc15b98535
khantext <justinlute@gmail.com>
2021-04-01 15:34:51 -0400

    fixed typos in init file comments

--- cvsser/__init__.py
+++ cvsser/__init__.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 
 """
-PyCVSS Library
+cvsser
 ~~~~~~~~~~~~~~~~~~~~~
-VS_CVSS is a library, written in Python, for manipulating CVSS vector strings, vector fields, and values.
+cvsser is a library, written in Python, for manipulating CVSS vector strings, vector fields, and values.
 
 :copyright: (c) 2021
 :license: Apache 3.0, see LICENSE for more details.
